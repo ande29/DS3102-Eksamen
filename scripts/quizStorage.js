7a51b417a7e33b9115cd222b819cbbce62ab0e00
OJoeyB <72861062+OJoeyB@users.noreply.github.com>
2021-11-03 16:25:15 +0100

    fix

---
+++ scripts/quizStorage.js
@@ -0,0 +1,12 @@
+const selectedGender = document.querySelector("#gender-select");
+const nameInput = document.querySelector("#name-input");
+const selectBtn = document.querySelector("#select-btn");
+
+
+let confirmInput = () => {
+    const name = nameInput.value;
+    const gender = selectedGender.value;
+
+    let message;
+ 
+}
