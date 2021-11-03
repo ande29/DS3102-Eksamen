d3a73697f703750fdba88724eb20115b4e8bf294
OJoeyB <72861062+OJoeyB@users.noreply.github.com>
2021-11-03 20:13:23 +0100

    quiz local storage

--- scripts/quizStorage.js
+++ scripts/quizStorage.js
@@ -3,34 +3,49 @@ const nameInput = document.querySelector("#name-input");
 const selectBtn = document.querySelector("#select-btn");
 const userOutput = document.querySelector(".user-output");
 
-
+let adminStorage = () => {
+    let addAdming = [
+    {
+        name:"admin",
+        gender: "netural"
+    }
+    ];
+    localStorage.setItem("users", JSON.stringify(addAdming));      
+}
+( // immediately triggered arrow function	
+	() => adminStorage()
+)();
 
 let addUser = () => {
     const name = nameInput.value;
     const gender = selectedGender.value;
     let message;
-
+    
     if(name.length > 0){
-        message = `
-        <p>${name} og du er en ${gender}</p>`;
-        storeUser;
+        message = `<p>${name} og du er en ${gender}</p>`;
+        storeUser(name, gender);
     }else{
         message = `<p style='color: red'> please select Gender and type in name`;
     }
+
     userOutput.innerHTML = message;
 
 
 }
 selectBtn.addEventListener("click", addUser)
 
-let storeUser = () => {
+let storeUser = (a,b) => {
     let userObj = [
         {
-            name: nameInput.value,
-            gender: selectedGender.value,
+            name: a,
+            gender: b,
         },
     ];
-        localStorage.push(userObj);
-        localStorage.setItem("Users", JSON.stringify(userObj));
+    
+    let users = JSON.parse(localStorage.getItem("users"));
+
+    users.push(userObj);
+
+    localStorage.setItem("users", JSON.stringify(userObj));
 }
 
