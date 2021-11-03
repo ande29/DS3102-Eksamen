7e1789cc7e4f814b064f6791f196f3e0e046e590
OJoeyB <72861062+OJoeyB@users.noreply.github.com>
2021-11-03 19:22:43 +0100

    quiz page update

--- scripts/quizStorage.js
+++ scripts/quizStorage.js
@@ -1,12 +1,36 @@
 const selectedGender = document.querySelector("#gender-select");
 const nameInput = document.querySelector("#name-input");
 const selectBtn = document.querySelector("#select-btn");
+const userOutput = document.querySelector(".user-output");
 
 
-let confirmInput = () => {
+
+let addUser = () => {
     const name = nameInput.value;
     const gender = selectedGender.value;
-
     let message;
- 
+
+    if(name.length > 0){
+        message = `
+        <p>${name} og du er en ${gender}</p>`;
+        storeUser;
+    }else{
+        message = `<p style='color: red'> please select Gender and type in name`;
+    }
+    userOutput.innerHTML = message;
+
+
 }
+selectBtn.addEventListener("click", addUser)
+
+let storeUser = () => {
+    let userObj = [
+        {
+            name: nameInput.value,
+            gender: selectedGender.value,
+        },
+    ];
+        localStorage.push(userObj);
+        localStorage.setItem("Users", JSON.stringify(userObj));
+}
+
