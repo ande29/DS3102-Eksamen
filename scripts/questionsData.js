b316440f1515cb3c0101f2e515e7994d1f43877b
OJoeyB <72861062+OJoeyB@users.noreply.github.com>
2021-11-10 11:00:00 +0100

    question fix

--- scripts/questionsData.js
+++ scripts/questionsData.js
@@ -1,84 +1,87 @@
 
 const athleteQustions = [
     {
-        number: "1", question: "Hvilken norsk idrettsstjerne spiller fotball i Tyskland?"
+        number: "1", question: "Hvilken norsk idrettsstjerne spiller fotball i Tyskland?",
+        answers:{
+            a: "Erlig Braut Haaland",
+            b: "Karsten Warholm"
+        },
+        correctAnswer: "a"
     },
     {
-        number: "2", question: "Hvilken idrett er Cecilia Brækhus blitt verdenskjent for?"
+        number: "2", question: "Hvilken idrett er Cecilia Brækhus blitt verdenskjent for?",
+            asnwers: {
+                a:"Golf",
+                b:"Boxing" 
+            },
+        correctAnswer: "b"
     },
     {
-        number: "3", question: "Hvor mange profesjonelle seiere har Suzann Pettersen i golf?"
+        number: "3", question: "Hvor mange profesjonelle seiere har Suzann Pettersen i golf?",
+        answers: {
+            a: "22",
+            b: "29"
+        },
+        correctAnswer: "a"
     },
     {
-        number: "4", question: "Hvor mange år var Magnus Carlsen da han ble stormester i sjakk?"
+        number: "4", question: "Hvor mange år var Magnus Carlsen da han ble stormester i sjakk?",
+        answers: {
+            a: "16",
+            b: "13"
+        },
+        correctAnswer:"b"
     },
     {
-        number: "5", question: "Hvilken norsk idrettsjerne vant VM-gull i friidrett?"
+        number: "5", question: "Hvilken norsk idrettsjerne vant VM-gull i friidrett?",
+        answers: {
+            a: "Karsten Warholm",
+            b: "Erling Braut Haaland"
+        },
+        correctAnswer: "a"
     },
 ];
 
-const athleteAnswers = [
-    {
-        suggestionOne: "Erling Braut Haaland", suggestionTwo: "Karsten Warholm"
-    },
-    {
-        suggestionOne: "Golf", suggestionTwo: "Boksing"
-    },
-    {
-        suggestionOne: "22 profesjonelle seiere", suggestionTwo: "29 profesjonelle seiere"
-    },
-    {
-        suggestionOne: "13", suggestionTwo: "11"
-    },
-    {
-        suggestionOne: "Karsten Warholm", suggestionTwo: "Erling Braut Haaland"
-    },
-];
-
-    export const athleteQandA = [...athleteQustions, ...athleteAnswers];
 
 const premierLeagueQustions = [
     {
-        number: "1", question: ""
-    },
-    {            
-        number: "2", question: ""  
-    },
-    {
-        number: "3", question: ""
-    },
-    {
-        number: "4", question: ""
+        number: "1", question: "",
+        answers: {
+            a: "",
+            b: ""
+        },
+        correctAnswer: ""
     },
     {
-        number: "5", question: ""
+        number: "1", question: "",
+        answers: {
+            a: "",
+            b: ""
+        },
+        correctAnswer: ""
     },
-    { 
-       number: "6", question: ""
+    {
+        number: "1", question: "",
+        answers: {
+            a: "",
+            b: ""
+        },
+        correctAnswer: ""
+    },
+    {
+        number: "1", question: "",
+        answers: {
+            a: "",
+            b: ""
+        },
+        correctAnswer: ""
+    },
+    {
+        number: "1", question: "",
+        answers: {
+            a: "",
+            b: ""
+        },
+        correctAnswer: ""
     }
 ];
-
-const premierLeagueAnswers = [
-    {
-        suggestionOne: "", suggestionTwo: ""
-    },
-    {
-        suggestionOne: "", suggestionTwo: ""
-    },
-    {
-        suggestionOne: "", suggestionTwo: ""
-    },
-    {
-        suggestionOne: "", suggestionTwo: ""
-    },
-    {
-        suggestionOne: "", suggestionTwo: ""
-    },
-    {
-        suggestionOne: "", suggestionTwo: ""
-    },
-];
-
-    export const premierLeagueQandA = [...premierLeagueQustions, ...premierLeagueAnswers];
-    
-//    export default {athleteQandA, premierLeagueQandA};
