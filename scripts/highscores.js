b8341e1ded13c4ad7220cde62dc2be50d09127b3
OJoeyB <72861062+OJoeyB@users.noreply.github.com>
2021-11-16 12:30:36 +0100

    sorting scores

--- scripts/highscores.js
+++ scripts/highscores.js
@@ -2,23 +2,22 @@ const highscoreTable = document.querySelector(".highscore-output");
 const highScoreBtn = document.querySelector(".highscore-btn");
 const playerHighscores = JSON.parse(localStorage.getItem("scores"));
 const crown = document.querySelector(".fa-crown");
+console.log(playerHighscores);
 
 
 const displayScore = () => {
     let htmlTxt = `<i class="fas fa-crown hidden"></i>`;
-
+    playerHighscores.sort((a,b) => b.score - a.score);
     playerHighscores.forEach(player => {
-            
-            htmlTxt += `
-            <p>${player.name} ${player.score}</p>
-            ` 
-            console.log(player)
-        });
-     //   crown.style.visibility = "visible";
+        htmlTxt += `
+        <p>${player.name} ${player.score}</p>
+        ` 
+        console.log(player)
+    });
+    //   crown.style.visibility = "visible";
         highscoreTable.innerHTML = htmlTxt;
 
 }
-
 highScoreBtn.addEventListener("click", () => {
     highscoreTable.classList.toggle("hidden");
    // crown.classList.toggle("hidden");
