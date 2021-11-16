4af989ee548c8caec9c1b02875c48e14b52e5622
OJoeyB <72861062+OJoeyB@users.noreply.github.com>
2021-11-16 15:46:18 +0100

    animation highscore

--- scripts/highscores.js
+++ scripts/highscores.js
@@ -5,20 +5,27 @@ const crown = document.querySelector(".fa-crown");
 console.log(playerHighscores);
 
 
+
 const displayScore = () => {
     let htmlTxt = `<i class="fas fa-crown hidden"></i>`;
     playerHighscores.sort((a,b) => b.score - a.score);
     playerHighscores.forEach(player => {
         htmlTxt += `
-        <p>${player.name} - ${player.score} pts</p>
+        <h4>${player.name} - ${player.score} pts</h4>
         ` 
-        console.log(player)
-    });
+      console.log(player)
+})
         highscoreTable.innerHTML = htmlTxt;
 
 }
+    let tween;
+
 highScoreBtn.addEventListener("click", () => {
     highscoreTable.classList.toggle("hidden");
     displayScore();
+    gsap.timeline()
+        .from(".highscore-output", {duration: 1, opacity: 0})
+        .from(".fa-crown", {opacity: 0, scale:0, ease:"back"})
+        .from("h4", {y:1000, opacity:0, stagger: 0.5, duration: 1, ease:"Power"})
 });
 
