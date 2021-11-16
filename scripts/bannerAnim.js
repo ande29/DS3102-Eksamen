f2cca4f6350bc7c8c7e3624c9ee52ee548d32cd4
ande29 <44792983+ande29@users.noreply.github.com>
2021-11-16 20:00:50 +0100

    rydd

--- scripts/bannerAnim.js
+++ scripts/bannerAnim.js
@@ -1,6 +1,7 @@
 let title = document.querySelector('.home-banner__title')
 let initials = document.querySelector('.initials')
-// home
+
+
 const easeInTimeLine = (a, b) => {
     let tl = gsap.timeline()
     tl.from(a, { duration: 2, opacity:0, scale:0, ease: " power3.out" });
